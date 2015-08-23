0b8c4a3851d582811ab543600ea3ca6a0bfe02b1
Laith <info@laithazzam.com>
2015-08-23 02:34:13 -0700

    v1 prototype

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -15,20 +15,51 @@
 		if(newMap){
 			newMap.kill();
 		}
-
-		width = window.innerWidth
-		height = window.innerHeight
+		$('#map').width(window.innerWidth*2.5);
+		width = window.innerWidth;
+		height = window.innerHeight;
 		newMap = Draggable.create("#map", {
 			type:"x,y", 
 			throwProps:true,
 			bounds:{top:0, left:0, width:width, height:height},
-			edgeResistance: .8,
+			edgeResistance: .97
 		});
+	}
+
+	function enter(){
+		var splash = $('#border');
+		var button = $('#enter-btn');
+		var intro = $('.intro-wrap');
+		var credits = $('.credits, .w');
+		button.click(function(){
+			console.log('click');
+			intro.fadeOut(function(){
+				splash.css({
+					backgroundColor: 'rgba(53,56,59,0)',
+					pointerEvents: 'none'
+				})
+
+				var tl = new TimelineLite();
+				var map = $('#map');
+
+				mapMidX = parseInt(map.width() / 2)-window.innerWidth/2;
+				mapMidY = parseInt(map.height() / 2)-window.innerHeight/2;
+				speed = 2;
+				tl.to(map, speed, {x:-mapMidX, y:-mapMidY, onComplete:introScrollDone});
+				tl.play();
+				function introScrollDone(){
+					credits.addClass('active');
+
+				}
+			});
+		});
+
 	}
 
 	/* trigger when page is ready */
 	$(document).ready(function (){
 		createMap();
+		enter();
 	});
 	
 	
